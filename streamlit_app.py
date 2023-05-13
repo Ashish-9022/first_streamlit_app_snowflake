e7d17893c4489d0cbc4c8d0649de9f6967b4bfb2
Ashish-9022 <55289068+Ashish-9022@users.noreply.github.com>
2023-05-13 18:55:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,8 @@ except URLError as err:
   st.error(err)
     
 
-
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 if st.button("Get Fruit data"):
-  my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
   my_cur = my_cnx.cursor()
   my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
   my_data_row = my_cur.fetchall()
